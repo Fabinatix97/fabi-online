82ac87f4369372fa41b71e18d3cacf8637d9711f
Fabinatix97 <fabian@fabi-online.de>
2024-10-30 17:30:33 +0100

    Adjust coloring of posts

--- components/Post.vue
+++ components/Post.vue
@@ -1,13 +1,13 @@
 <template>
     <div v-for="post in props.posts" :key="post.slug"
-    class="bg-white rounded-lg shadow-md overflow-hidden hover:opacity-75">
+    class="bg-card rounded-lg shadow-md overflow-hidden">
         <NuxtLink :to="post._path">
-            <img :src="`/img/blog/${post.cover}`" alt="Blog Post Cover Image" class="w-full h-48 object-cover">
+            <img :src="`/img/blog/${post.cover}`" alt="Blog Post Cover Image" class="w-full h-48 object-cover hover:opacity-80">
         </NuxtLink>
         <div class="p-6">
             <h2 class="text-xl font-bold mb-2">{{ post.title }}</h2>
-            <p class="text-gray-700 mb-4">{{ post.description }}</p>
-            <NuxtLink :to="post._path" class="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded float-right mb-8">
+            <p class="text-cardtext mb-4">{{ post.description }}</p>
+            <NuxtLink :to="post._path" class="inline-block bg-button hover:bg-buttonhover text-white py-2 px-4 rounded float-right mb-8">
                 Read More
             </NuxtLink>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,15 @@ export default {
     extend: {
       colors: {
         button: "var(--button)",
+        buttonhover: "var(--buttonhover)",
         backgreen: "var(--backgreen)",
         backblue: "var(--backblue)",
         body: "var(--body)",
         main: "var(--main)",
         border: "var(--border)",
         dots: "var(--dots)",
+        card: "var(--card)",
+        cardtext: "var(--cardtext)",
       },
     },
   },
