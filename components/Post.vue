76f66320bd0d07ef9b27c0bc03e2faa4ce09727f
Fabinatix97 <fabian@fabi-online.de>
2024-10-30 17:51:29 +0100

    Fix nuxt link

--- components/Post.vue
+++ components/Post.vue
@@ -1,17 +1,17 @@
 <template>
-    <div v-for="post in props.posts" :key="post.slug"
+    <NuxtLink :to="post._path" v-for="post in props.posts" :key="post.slug"
     class="bg-card rounded-lg shadow-md overflow-hidden">
-        <NuxtLink :to="post._path">
+        <div>
             <img :src="`/img/blog/${post.cover}`" alt="Blog Post Cover Image" class="w-full h-48 object-cover hover:opacity-80">
-        </NuxtLink>
+        </div>
         <div class="p-6">
             <h2 class="text-xl font-bold mb-2">{{ post.title }}</h2>
             <p class="text-cardtext mb-4">{{ post.description }}</p>
-            <NuxtLink :to="post._path" class="inline-block bg-button hover:bg-buttonhover text-white py-2 px-4 rounded float-right mb-8">
+            <div class="inline-block bg-button hover:bg-buttonhover text-white py-2 px-4 rounded float-right mb-8">
                 Read More
-            </NuxtLink>
+            </div>
         </div>
-    </div>
+    </NuxtLink>
 </template>
 
 <script setup>
