9d634f093d0cfff171f3f615c0595d55692d0827
Fabinatix97 <fabian@fabi-online.de>
2024-11-01 22:16:27 +0100

    Add animated theme icons and hover color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,26 +1,27 @@
 <template>
-    <nav class="navbar bg-main border border-solid border-border">
+    <nav class="navbar bg-body border border-solid border-border">
         <ul class="nav-links">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/about">Über mich</NuxtLink></li>
-            <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-            <li><NuxtLink to="/impressum">Impressum</NuxtLink></li>
+            <li :class="{ 'active-link': isActiveRoute('/') }"><NuxtLink to="/">Home</NuxtLink></li>
+            <li :class="{ 'active-link': isActiveRoute('/about') }"><NuxtLink to="/about">About</NuxtLink></li>
+            <li :class="{ 'active-link': isActiveRoute('/blog') }"><NuxtLink to="/blog">Blog</NuxtLink></li>
+            <li :class="{ 'active-link': isActiveRoute('/impressum') }"><NuxtLink to="/impressum">Impressum</NuxtLink></li>
             <li>
                 <a href="https://de.linkedin.com/in/fabian-weiss" class="text-text" target="_blank" rel="noopener noreferrer">
-                <Icon name="mdi:linkedin" size="1.4em" />
+                    <Icon name="mdi:linkedin" size="1.4em" />
                 </a>
             </li>
             <li>
                 <a href="https://github.com/Fabinatix97" target="_blank" rel="noopener noreferrer">
-                <Icon name="mdi:github" size="1.4em" />
+                    <Icon name="mdi:github" size="1.4em" />
                 </a>
             </li>
             <li>
-                <button @click="$toggleTheme()" class="theme-toggle-button">
+                <button @click="$toggleTheme()" class="text-text hover:text-button">
                     <ClientOnly>
-                        <img :src="themeIconSrc" 
-                            :alt="theme.valueOf === 'light' ? 'Mond-Icon' : 'Sonnen-Icon'" 
-                            class="theme-icon" />
+                        <Icon 
+                            :name="themeIconName" 
+                            size="1.4em"
+                        />
                     </ClientOnly>
                 </button>
             </li>
@@ -30,13 +31,18 @@
 
 <script setup>
 import { computed } from 'vue';
-import { useNuxtApp } from '#app';
+import { useNuxtApp, useRoute } from '#app';
 
 const { $theme, $toggleTheme } = useNuxtApp();
 const theme = $theme;
+const route = useRoute();
 
-const themeIconSrc = computed(() => {
-    return theme.value === 'light' ? '/img/moon-icon.png' : '/img/sun-icon.png';
+const isActiveRoute = (path) => {
+    return route.path === path;
+};
+
+const themeIconName = computed(() => {
+    return theme.value === 'light' ? 'line-md:moon-filled-loop' : 'line-md:sunny-filled-loop';
 });
 </script>
 
@@ -63,23 +69,18 @@ const themeIconSrc = computed(() => {
     margin-right: 0;
 }
 .nav-links li:hover::before {
-    color: #3498db;
-}
-.theme-toggle-button {
-    background: none;
-    border: none;
-    cursor: pointer;
-    padding: 0;
-}
-.theme-icon {
-    width: 20px;
-    height: 20px;
-    opacity: 0.85;
+    color: #075985;
 }
 a {
     text-decoration: none;
 }
 a:hover {
-    color: #0E74AF;
+    color: #075985;
+    transition: all 0.3s;
+}
+
+.active-link {
+    color: #075985;
+    border-bottom: 2px solid #075985;
 }
 </style>
