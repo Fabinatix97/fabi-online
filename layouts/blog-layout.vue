202f4c78ecb1915d51f1fd8fd8d3d9260f8679b6
Fabinatix97 <fabian@fabi-online.de>
2024-11-01 22:14:07 +0100

    Add blog layout

---
+++ layouts/blog-layout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="posts">
+        <nuxt-page />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.posts {
+    margin: 20px auto;
+    padding: 30px;
+    max-width: 1000px;
+    width: 100%;
+}
+</style>
