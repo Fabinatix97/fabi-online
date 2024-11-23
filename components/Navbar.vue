757183294f06cc615086b209ed04a3ac456a5a22
Fabinatix97 <fabian@fabi-online.de>
2024-11-23 16:13:34 +0100

    Decoupling navbar into mobile and regular component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,156 +1,42 @@
 <template>
-    <Disclosure as="nav" class="navbar border-b-2 border-mainborder hyphens-none" v-slot="{ open, close }">
-        <!-- Mobile menu button-->
-        <div class="flex items-start">
-            <DisclosureButton
-                class="sm:hidden rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                <span class="sr-only">Open main menu</span>
-                <Icon name="line-md:close-to-menu-alt-transition" v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-                <Icon name="line-md:menu-to-close-alt-transition" v-else class="block h-6 w-6" aria-hidden="true" />
-            </DisclosureButton>
-        </div>
-        <div :class="[open ? 'flex w-full justify-between' : '', 'flex w-full justify-end']">
-            <DisclosurePanel class="content-center sm:hidden">
-                <div class="space-y-1 pb-3">
-                    <NuxtLink 
-                        v-for="item in navigation" 
-                        :key="item.name" 
-                        as="a" 
-                        :to="item.href"
-                        :class="[isActiveRoute(item.href) ? 'bg-buttonhover text-buttontext hover:text-white' : 'text-text hover:bg-body hover:text-button', 'block rounded-md px-3 py-2 text-base font-medium']"
-                        :aria-current="item.current ? 'page' : undefined"
-                        @click="close">
-                        {{ item.name }}
-                    </NuxtLink>
-                </div>
-                <div class="flex px-2">
-                    <div class="mr-4">
-                        <a href="https://de.linkedin.com/in/fabian-weiss" class="transition-all duration-300 hover:text-buttonhover" target="_blank" rel="noopener noreferrer">
-                            <Icon name="mdi:linkedin" size="2em" />
-                        </a>
-                    </div>
-                    <div>
-                        <a href="https://github.com/Fabinatix97" class="transition-all duration-300 hover:text-buttonhover" target="_blank" rel="noopener noreferrer">
-                            <Icon name="mdi:github" size="2em" />
-                        </a>
-                    </div>
-                </div>
-            </DisclosurePanel>
-            <div v-if="!open" class="flex sm:hidden">
-                <button @click="$toggleTheme()" class="text-text hover:text-button">
-                    <ClientOnly>
-                        <Icon 
-                            :name="themeIconName" 
-                            size="1.4em"
-                        />
-                    </ClientOnly>
-                </button>
-            </div>
-        </div>
-        <!-- Regular menu -->
-        <div class="hidden sm:ml-6 sm:block">
-            <ul class="nav-links">
-                <li :class="{ 'active-link': isActiveRoute('/') }"><NuxtLink to="/">Home</NuxtLink></li>
-                <li :class="{ 'active-link': isActiveRoute('/about') }"><NuxtLink to="/about">About</NuxtLink></li>
-                <li :class="{ 'active-link': isActiveRoute('/blog') }"><NuxtLink to="/blog">Blog</NuxtLink></li>
-                <li :class="{ 'active-link': isActiveRoute('/impressum') }"><NuxtLink to="/impressum">Impressum</NuxtLink></li>
-                <li>
-                    <a href="https://de.linkedin.com/in/fabian-weiss" class="text-text" target="_blank" rel="noopener noreferrer">
-                        <Icon name="mdi:linkedin" size="1.4em" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/Fabinatix97" target="_blank" rel="noopener noreferrer">
-                        <Icon name="mdi:github" size="1.4em" />
-                    </a>
-                </li>
-                <li>
-                    <button @click="$toggleTheme()" class="text-text hover:text-button">
-                        <ClientOnly>
-                            <Icon 
-                                :name="themeIconName" 
-                                size="1.4em"
-                            />
-                        </ClientOnly>
-                    </button>
-                </li>
-            </ul>
-        </div>
-    </Disclosure>
-</template>
-
-<script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
-import { computed } from 'vue';
-import { useNuxtApp, useRoute } from '#app';
-
-const { $theme, $toggleTheme } = useNuxtApp();
-const theme = $theme;
-const route = useRoute();
-
-const navigation = [
-    { name: 'Home', href: '/', current: route.name == 'index' },
-    { name: 'About', href: '/about', current: route.name.includes('about') },
-    { name: 'Blog', href: '/blog', current: route.name == 'blog' },
-    { name: 'Impressum', href: '/impressum', current: route.name == 'uses' },
-]
-
-const isActiveRoute = (path) => {
-    return route.path === path;
-};
-
-const themeIconName = computed(() => {
-    return theme.value === 'light' ? 'line-md:moon-filled-loop' : 'line-md:sunny-filled-loop';
-});
-</script>
-
-<style lang="scss" scoped>
-.disclosure-panel {
-    border-radius: 0.5rem;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    z-index: 1000;
-}
-.navbar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    display: flex;
-    padding: 1rem;
-    font-weight: 500;
-    z-index: 1000;
-    background-color: var(--navbar);
-    backdrop-filter: blur(10px);
-
-    @media screen and (min-width: 640px) {
-        justify-content: flex-end;
-    }
-}
-.nav-links {
-    list-style: none;
-    display: flex;
-    gap: 1rem;
-    li {
-        &:last-child {
-            margin-right: 0;
+    <div>
+      <NavbarRegular v-if="isDesktop" />
+      <NavbarMobile v-else />
+    </div>
+  </template>
+  
+  <script>
+  import NavbarRegular from './NavbarRegular.vue';
+  import NavbarMobile from './NavbarMobile.vue';
+  
+  export default {
+    components: {
+      NavbarRegular,
+      NavbarMobile
+    },
+    data() {
+      return {
+        isDesktop: false
+      };
+    },
+    mounted() {
+      if (typeof window !== 'undefined') {
+        this.isDesktop = window.innerWidth >= 640;
+        window.addEventListener('resize', this.handleResize);
+      }
+    },
+    beforeDestroy() {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', this.handleResize);
+      }
+    },
+    methods: {
+      handleResize() {
+        if (typeof window !== 'undefined') {
+          this.isDesktop = window.innerWidth >= 640;
         }
-        &::before {
-            transition: color 0.3s;
-            margin-right: 10px;
-            transform: scaleX(0);
-            transform-origin: bottom right;
-        }
-        &:hover::before {
-            color: var(--buttonhover);
-        }
+      }
     }
-    a:hover {
-        color: var(--buttonhover);
-        transition: all 0.3s;
-    }
-}
-.active-link {
-    color: var(--button);
-    border-bottom: 2px solid var(--button);
-}
-</style>
+  };
+  </script>
+  
---
+++ components/NavbarRegular.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="fixed">
+        <div as="nav" class="navbar hyphens-none" :class="themeClass">
+            <div class="flex justify-between border-solid">
+                <div>
+                    <ul class="nav-links">
+                        <li :class="{ 'active-link': isActiveRoute('/') }"><NuxtLink to="/">Home</NuxtLink></li>
+                        <li :class="{ 'active-link': isActiveRoute('/about') }"><NuxtLink to="/about">About</NuxtLink></li>
+                        <li :class="{ 'active-link': isActiveRoute('/blog') }"><NuxtLink to="/blog">Blog</NuxtLink></li>
+                        <li :class="{ 'active-link': isActiveRoute('/impressum') }"><NuxtLink to="/impressum">Impressum</NuxtLink></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="nav-links">
+                        <li>
+                            <a href="https://de.linkedin.com/in/fabian-weiss" class="text-text" target="_blank" rel="noopener noreferrer">
+                                <Icon name="mdi:linkedin" size="1.4em" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Fabinatix97" target="_blank" rel="noopener noreferrer">
+                                <Icon name="mdi:github" size="1.4em" />
+                            </a>
+                        </li>
+                        <li>
+                            <button @click="$toggleTheme()" class="text-text hover:text-button">
+                                <ClientOnly>
+                                    <Icon 
+                                        :name="themeIconName" 
+                                        size="1.4em"
+                                    />
+                                </ClientOnly>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useNuxtApp, useRoute } from '#app';
+
+const { $theme, $toggleTheme } = useNuxtApp();
+const theme = $theme;
+const route = useRoute();
+
+const isActiveRoute = (path) => {
+    return route.path === path;
+};
+
+const themeIconName = computed(() => {
+    return theme.value === 'light' ? 'line-md:moon-filled-loop' : 'line-md:sunny-filled-loop';
+});
+
+const themeClass = computed(() => {
+  return theme.value === 'light' ? 'border-solid border-[1px] border-mainborder' : 'border-none';
+});
+
+</script>
+
+<style lang="scss" scoped>
+.fixed {
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    margin-top: 20px;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1000;
+}
+
+.navbar {
+    width: 800px;
+    padding: 1rem;
+    font-weight: 500;
+    background-color: var(--navbar);
+    backdrop-filter: blur(10px);
+    border-radius: 10px;
+}
+
+.nav-links {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    li {
+        &:last-child {
+            margin-right: 0;
+        }
+        &::before {
+            transition: color 0.3s;
+            margin-right: 10px;
+            transform: scaleX(0);
+            transform-origin: bottom right;
+        }
+        &:hover::before {
+            color: var(--buttonhover);
+        }
+    }
+    a:hover {
+        color: var(--buttonhover);
+        transition: all 0.3s;
+    }
+}
+
+.active-link {
+    color: var(--button);
+    position: relative;
+}
+
+.active-link::after {
+    content: '';
+    position: absolute;
+    bottom: -5px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background-color: var(--button);
+}
+</style>
