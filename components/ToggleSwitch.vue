dec5fbad17705a5ee857ed0cef8f114bb735339d
Fabinatix97 <fabian@fabi-online.de>
2024-11-25 17:47:22 +0100

    Minor color adjustment

--- components/ToggleSwitch.vue
+++ components/ToggleSwitch.vue
@@ -25,45 +25,45 @@ const toggle = () => {
 
 <style lang="scss" scoped>
 .toggle-container {
-  display: flex;
-  align-items: center;
-  cursor: pointer;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
 }
 
 .toggle-switch {
-  width: 90px;
-  height: 40px;
-  background-color: var(--primary);
-  border-radius: 20px;
-  position: relative;
-  margin: 0 10px;
+    width: 90px;
+    height: 40px;
+    background-color: var(--primary);
+    border-radius: 20px;
+    position: relative;
+    margin: 0 10px;
 
-  &.active {
-    .toggle-circle {
-      transform: translateX(50px);
+    &.active {
+        .toggle-circle {
+            transform: translateX(50px);
+        }
     }
-  }
 
-  .toggle-circle {
-    color: var(--primary);
-    width: 32px;
-    height: 32px;
-    background-color: var(--main);
-    border-radius: 50%;
-    position: absolute;
-    top: 4px;
-    left: 4px;
-    transition: transform 0.3s;
-  }
+    .toggle-circle {
+        color: var(--primary);
+        width: 32px;
+        height: 32px;
+        background-color: var(--main);
+        border-radius: 50%;
+        position: absolute;
+        top: 4px;
+        left: 4px;
+        transition: transform 0.3s;
+    }
 }
 
 span {
-  font-size: 16px;
-  font-weight: bold;
-  transition: color 0.3s;
+    font-size: 16px;
+    font-weight: bold;
+    transition: color 0.3s;
 
-  &.active-text {
-    color: var(--heading);
-  }
+    &.active-text {
+        color: var(--heading);
+    }
 }
 </style>
