05fd0777c464fa35d1323c6f2b42e5f5ac36eec1
Fabinatix97 <fabian@fabi-online.de>
2024-11-26 15:25:41 +0100

    Moving spotlight effect to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,19 @@
 <template>
-    <div class="flex justify-center mt-24 mx-5 mb-10">
+    <div class="flex justify-center mt-40 mx-5 mb-10">
         <div class="w-[800px]">
+            <client-only>
+                <svg v-if="theme === 'dark'" width="400" height="300" viewBox="0 0 10 11" class="absolute -z-10 blur-[120px]">
+                    <circle cx="2" cy="2" r="5" fill="var(--primary)" />
+                </svg>
+            </client-only>
             <nuxt-page />
         </div>
     </div>
 </template>
+
+<script setup>
+import { useNuxtApp } from '#app';
+
+const { $theme } = useNuxtApp();
+const theme = $theme;
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,5 @@
 <template>
-    <client-only>
-        <svg v-if="theme === 'dark'" width="400" height="300" viewBox="0 0 10 11" class="absolute -z-10 blur-[120px]">
-            <circle cx="2" cy="2" r="5" fill="var(--primary)" />
-        </svg>
-    </client-only>
-
+    
     <h1>Meine Geschichte</h1>
 
     <div class="grid gap-4 grid-cols-1 sm:grid-cols-3">
