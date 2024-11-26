6a9810f8095ba5320475c5be35ede0c32c4f4aca
Fabinatix97 <fabian@fabi-online.de>
2024-11-26 14:09:33 +0100

    Add projects component

---
+++ components/Projects.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="grid grid-cols-2 gap-4">
+        <div v-for="project in projects" class="flex flex-col justify-between bg-main border-[1px] border-border rounded-[10px]">
+            <div>
+                <NuxtLink :to="project.link">
+                    <img
+                        :src="`${project.image}`"
+                        alt="Project Image"
+                        class="w-full h-48 object-cover rounded-t-lg hover:opacity-80 relative"
+                    />
+                </NuxtLink>
+                <div class="flex flex-col px-6">
+                    <NuxtLink :to="project.link" class="flex">
+                        <h4 class="flex hover:text-primaryhover">{{ project.name }}</h4>
+                    </NuxtLink>
+                    <div class="flex flex-wrap justify-start gap-1">
+                        <div v-for="tech in project.technologies" class="py-1 px-3 bg-body text-sm rounded-lg border-[1px] border-border">
+                                {{ tech }}
+                        </div>
+                    </div>
+                    <p class="text-base mb-4">{{ project.description }}</p>
+                </div>
+            </div>
+            <div class="flex justify-end pr-6 pb-4">
+                <NuxtLink :to="project.link" class="flex">
+                        <Icon name="mdi:github" size="1.5em" class="hover:text-primaryhover" />
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="js">
+
+const projects = [
+    {
+        name: 'VueDoList',
+        image: '/img/vuedo.png',
+        link: 'https://github.com/Fabinatix97',
+        technologies: ['Vue.js', 'Javascript'],
+        description: 'Schlanke und minimalistische Todo-Liste mit Fokus auf dem Wesentlichen.'
+    },
+    {
+        name: 'fabi-online',
+        image: '/img/fabionline.png',
+        link: 'https://github.com/Fabinatix97',
+        technologies: ['Nuxt', 'Javascript', 'Tailwindcss'],
+        description: 'Selbstentwickelte Website als Ablöse für Wordpress-Blog. Die Basis bietet Nuxt, ein Vue.js Framework, das Flexibilität und moderne Features liefert.'
+    }
+]
+</script>
