393af0545d1c28f7e99c74d7c60cae32a967bcb3
Fabinatix97 <fabian@fabi-online.de>
2024-11-26 15:05:08 +0100

    Add header, description and search bar dummy

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,19 @@
 <template>
-    <section>
-        <Cards :posts="posts"/>
-    </section>
+    <h1>Blog</h1>
+    <p class="mb-0">Hier teile ich meine Gedanken und Erfahrungen zu allem, was mich gerade beschäftigt.</p>
+    <div class="flex items-center h-20">
+        <div class="flex gap-1 bg-main text-border p-2 rounded-full border-[1px] border-border">
+            <div class="flex">
+                <Icon name="mdi:search" size="1.5em" />
+            </div>
+            <div class="flex w-60">Artikel suchen</div>
+        </div>
+    </div>
+    <Cards :posts="posts"/>
 </template>
   
 <script setup>
 const { data: posts } = await useAsyncData('posts', () =>
     queryContent('/blog').find()
 );
-definePageMeta({
-    layout: 'bloglayout'
-});
 </script>
