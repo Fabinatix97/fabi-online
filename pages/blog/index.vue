d7bbcdebd3b087d6b8e97b871da3d7a6e30ffe70
Fabinatix97 <fabian@fabi-online.de>
2024-11-26 15:24:24 +0100

    Improve contrast within searchbar

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
     <h1>Blog</h1>
     <p class="mb-0">Hier teile ich meine Gedanken und Erfahrungen zu allem, was mich gerade beschäftigt.</p>
     <div class="flex items-center h-20">
-        <div class="flex gap-1 bg-main text-border p-2 rounded-full border-[1px] border-border">
+        <div class="flex gap-1 bg-main text-info p-2 rounded-full border-[1px] border-border">
             <div class="flex">
                 <Icon name="mdi:search" size="1.5em" />
             </div>
