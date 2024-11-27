19fa81ce9af5bc5386158184774fc88a5e40760a
Fabinatix97 <fabian@fabi-online.de>
2024-11-27 21:34:34 +0100

    Add wide layout

---
+++ layouts/widelayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wide">
+        <nuxt-page />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.wide {
+    padding: 0 40px 40px 40px;
+    width: 100%;
+    margin: auto;
+}
+
+@media (max-width: 640px) {
+    .wide {
+        padding: 0 20px 20px 20px;
+    }
+}
+</style>
