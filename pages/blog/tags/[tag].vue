afee9d8b41dcc2072bf7cf2b9a66421d5b6e36cc
Fabinatix97 <fabian@fabi-online.de>
2024-11-29 13:39:09 +0100

    Update tag page

--- pages/blog/tags/[tag].vue
+++ pages/blog/tags/[tag].vue
@@ -1,12 +1,23 @@
 <template>
-    <h1>#{{ tag }}</h1>
-    <section>
-        <Cards :posts="filteredPosts"/>
-    </section>
+    <div class="flex justify-center mt-32 mx-5 mb-10">
+        <div class="w-[800px]">
+            <client-only>
+                <svg v-if="theme === 'dark'" width="400" height="300" viewBox="0 0 10 11" class="absolute -z-10 blur-[120px]">
+                    <circle cx="2" cy="2" r="5" fill="var(--primary)" />
+                </svg>
+            </client-only>
+            <h1>#{{ tag }}</h1>
+        </div>
+    </div>
+    <Gallery :posts="posts"/>
 </template>
 
 <script setup>
+import { useNuxtApp } from '#app';
 import { useRoute } from 'vue-router';
+
+const { $theme } = useNuxtApp();
+const theme = $theme;
 
 const route = useRoute();
 const tag = route.params.tag;
@@ -18,6 +29,6 @@ const { data: posts } = await useAsyncData('posts', () =>
 const filteredPosts = computed(() => posts.value);
 
 definePageMeta({
-    layout: 'bloglayout'
+    layout: 'widelayout'
 });
 </script>
