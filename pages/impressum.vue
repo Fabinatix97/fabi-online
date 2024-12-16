caf6b265e77a95751922bbf13ca06e4545f8ba50
Fabinatix97 <fabian@fabi-online.de>
2024-12-16 13:48:56 +0100

    Add content and styling to Impressum page

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,3 +1,20 @@
 <template>
-    <ContentDoc />
+    <ContentDoc class="impressum"/>
 </template>
+
+<style lang="scss">
+.impressum {
+    ul {
+        margin-left: 3rem;
+        list-style-type: disc;
+    }
+    pre {
+        background-color: var(--codebg);
+        color: var(--codetext);
+        border-radius: 15px;
+        padding: 20px;
+        font-size: medium;
+        font-family: 'Roboto Mono';
+    }
+}
+</style>
