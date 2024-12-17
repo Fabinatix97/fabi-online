cdb3e7661e247a92ef5783547fa5ecf3a6d5a305
Fabinatix97 <fabian@fabi-online.de>
2024-12-17 09:42:02 +0100

    Add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/fabian-weiss/'
+    baseURL: '/fabian-weiss/',
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: 'Fabian Weiß' }
+      ]
+    }
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
