c8a0c874fc29d520165b436bf74806369a560b95
Fabinatix97 <fabian@fabi-online.de>
2024-12-23 17:25:24 +0100

    Enhance accessibility of ToggleSwitch component with ARIA attributes

--- components/ToggleSwitch.vue
+++ components/ToggleSwitch.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="toggle-container" @click="toggle">
-        <span :class="{ 'active-text': !isDevOps }" class="text-info">Pilot</span>
+    <div 
+        class="toggle-container" 
+        @click="toggle" 
+        role="switch" 
+        :aria-checked="isDevOps" 
+        :aria-labelledby="isDevOps ? 'devops-label' : 'pilot-label'"
+    >
+        <span :class="{ 'active-text': !isDevOps }" class="text-info" id="pilot-label">Pilot</span>
         <div class="toggle-switch" :class="{ 'active': isDevOps }">
             <div class="toggle-circle">
                 <Icon v-if="isDevOps" name="mdi:code" class="text-primary absolute left-[5px] top-[4px]" size="1.4em"/>
                 <Icon v-else name="ri:plane-fill" class="text-primary absolute left-[4px] top-[4px]" size="1.4em"/>
             </div>
         </div>
-        <span :class="{ 'active-text': isDevOps }" class="text-info">DevOps</span>
+        <span :class="{ 'active-text': isDevOps }" class="text-info" id="devops-label">DevOps</span>
     </div>
 </template>
 
