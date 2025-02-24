ae387e0f4958171299c03621302e41a5e7ac89f9
Fabinatix97 <fabian@fabi-online.de>
2025-02-24 07:15:51 +0100

    Add Prettier configuration and integrate with ESLint for improved code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      ...eslintPluginPrettierRecommended.rules,
+    },
+  },
+]);
