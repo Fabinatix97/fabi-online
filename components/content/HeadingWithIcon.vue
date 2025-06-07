0605149636355ea05d47d5cd49ab5f347f5535a0
Fabinatix97 <fabian@fabi-online.de>
2025-06-07 10:59:11 +0200

    Add HeadingWithIcon component

---
+++ components/content/HeadingWithIcon.vue
@@ -0,0 +1,21 @@
+<script setup>
+// TODO: Update TOC logic to support HeadingWithIcon components
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: [String, Number],
+    default: 2,
+    validator: v => [1,2,3,4,5,6].includes(+v)
+  }
+})
+</script>
+
+<template>
+  <component :is="`h${level}`" class="flex items-center gap-2">
+    <Icon :name="icon" />
+    <slot mdc-unwrap="p" />
+  </component>
+</template>
